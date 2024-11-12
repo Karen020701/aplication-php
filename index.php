8538bd36d4cfa846055619134ade2a942b158323
Karen020701 <karenchicaiza989@gmail.com>
2024-11-12 10:05:34 -0500

    feat: :bug: update header

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -5,7 +6,13 @@
     <title>Hola Mundo en PHP</title>
     <style>
         body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
-        h1 { color: #4CAF50; }
+        h1 { color: #000000; }
     </style>
 </head>
+<body>
+    <h1>Hola Mundo lenguaje PHP !</h1>
+    <?php
+        echo "<p>Hoy es " . date("Y/m/d") . "</p>";
+    ?>
+</body>
 </html>
